228f70a2788b457eb27e7ff1e52e8a976d4fb1cf
Dao Hoang Son <daohoangson@gmail.com>
2023-10-09 00:53:34 +0700

    Configure Bucketeer ($5/mo) for storage

--- appwrite/prepare-env.sh
+++ appwrite/prepare-env.sh
@@ -17,10 +17,26 @@ if [ ! -z "$REDIS_URL" ]; then
   export _APP_REDIS_PASS=$(php -r 'echo parse_url(getenv("REDIS_URL"), PHP_URL_PASS);')
 fi
 
+if [ ! -z "$S3_AWS_ACCESS_KEY_ID" ]; then
+  export _APP_STORAGE_DEVICE="s3"
+  export _APP_STORAGE_S3_ACCESS_KEY=$S3_AWS_ACCESS_KEY_ID
+  export _APP_STORAGE_S3_SECRET=$S3_AWS_SECRET_ACCESS_KEY
+  export _APP_STORAGE_S3_REGION=$S3_AWS_REGION
+  export _APP_STORAGE_S3_BUCKET=$S3_BUCKET_NAME
+fi
+
 # some sane default for Heroku environments
+export _APP_STORAGE_ANTIVIRUS=${_APP_STORAGE_ANTIVIRUS:-'disabled'}
+export _APP_STORAGE_LIMIT=${_APP_STORAGE_LIMIT:-'10000000'}
+export _APP_STORAGE_PREVIEW_LIMIT=${_APP_STORAGE_PREVIEW_LIMIT:-'20000000'}
 export _APP_WORKER_PER_CORE=${_APP_WORKER_PER_CORE:-'1'}
 
-env | grep _APP_ | grep -v PASS | sort
+# print prepared environment variables for verification
+env |
+  grep _APP_ |
+  grep -v PASS |
+  grep -v SECRET |
+  sort
 
 # disable OPcache because Redis->pconnect doesn't work on Heroku (no idea why)
 rm -f /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
